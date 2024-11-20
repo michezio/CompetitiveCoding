8764f28854fa2961d381381a13186d2a68573820
michezio <33634641+michezio@users.noreply.github.com>
2024-11-21 00:10:28 +0100

    some deletions

--- Google/KickStart/KickStart 2020/B_plates.py
+++ Google/KickStart/KickStart 2020/B_plates.py
@@ -1,25 +1,3 @@
-def compute(budget, costs):
-    costs.sort()
-    houses = []
-    total = 0
-    while(costs):
-        total += costs[0]
-        if (total > budget):
-            break
-        else:
-            houses.append(costs.pop(0))
-    return len(houses)
-
-results = []
-
-cases_num = int(input())
-
-for i in range(cases_num):
-    num_houses, budget = [int(s) for s in input().split(" ")]
-    costs = [int(c) for c in input().split(" ")]
-    results.append(compute(budget, costs))
-
-case = 1
-for num in results:
-    print("Case #{}: {}".format(case, num))
-    case += 1
+"""
+CODE MISSING
+"""
--- LeetCode/30 Day LeetCoding Challenge/4_move_zeroes.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def moveZeroes(self, nums):
-        set_position = 0
-        for i in range(set_position, len(nums)):
-            if nums[i] != 0:
-                temp = nums[i]
-                nums[i] = nums[set_position]
-                nums[set_position] = temp
-                set_position += 1
